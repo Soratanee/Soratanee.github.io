c0b7c7ddb0cb93037b14e5efcd2cdc0843b40c84
Soratanee <oerica8864@gmail.com>
2023-02-23 20:56:58 -0500

    added

--- examples/bootstrapExamples/js/primitiveDataTypes.js
+++ examples/bootstrapExamples/js/primitiveDataTypes.js
@@ -4,3 +4,15 @@
 
 // Data Type: String (text)
 console.log("Hello World!");
+
+// Data Type: Number
+console.log(4)
+console.log(10000);
+
+// Math operator
+// additition (+), subtraction (-)
+// multiplication (*), division (/)
+console.log(10 + 5);
+console.log(20 - 12);
+console.log(3 * 4);
+console.log(10 /2);
