14295593ec16c9c27ec605e992305a253b09cfc9
Soratanee <oerica8864@gmail.com>
2023-02-23 21:04:58 -0500

    added

--- examples/bootstrapExamples/js/primitiveDataTypes.js
+++ examples/bootstrapExamples/js/primitiveDataTypes.js
@@ -3,7 +3,7 @@
 // to the Chrome Developer Tools' Console
 
 // Data Type: String (text)
-console.log("Hello World!");
+console.log("Hello World");
 
 // Data Type: Number
 console.log(4)
@@ -16,3 +16,7 @@ console.log(10 + 5);
 console.log(20 - 12);
 console.log(3 * 4);
 console.log(10 /2);
+
+// String operator
+// concatenation (+) (putting two Strings together)
+console.log("Hello" + "World" + "!!!!");
