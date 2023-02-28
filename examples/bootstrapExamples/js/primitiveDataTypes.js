2b6e632bc62ea3e23b345952339da294318a7933
Soratanee <oerica8864@gmail.com>
2023-02-27 21:18:41 -0500

    added

--- examples/bootstrapExamples/js/primitiveDataTypes.js
+++ examples/bootstrapExamples/js/primitiveDataTypes.js
@@ -20,3 +20,24 @@ console.log(10 /2);
 // String operator
 // concatenation (+) (putting two Strings together)
 console.log("Hello" + "World" + 64);
+
+// Data Type: Boolean (true/false)
+// equivalence (==) (is equal to)
+console.log(5 != 15); // true
+console.log(5 !=5); // false
+
+//less than (<)
+console.log(5 < 10); // true
+console.log(10 < 5); // false
+console.log(5 < 5); // false
+
+// greater than (>)
+console.log(5 > 10); // false
+console.log(10 > 5); // true
+console.log(5 > 5); // false
+
+// greater than or equal to (>=)
+// less than or equal to (<=)
+console.log(5 >=10); // false
+console.log(10 >= 5); // true
+console.log(5 >= 5); // true
